a8122b6dd22bac2be37384bf68f0cd478541e123
ballfish <lili668668@gmail.com>
2025-08-23 15:37:53 +0800

    WIP: test lottie

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Vue3Lottie from 'vue3-lottie'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vue3Lottie)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,11 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
+    <Vue3Lottie
+      animationLink="https://assets2.lottiefiles.com/packages/lf20_GbabwrUY2k.json"
+      :height="200"
+      :width="200"
+    />
     <TheWelcome />
   </main>
 </template>
