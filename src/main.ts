f57c293bd9e75928a9df488610b286936a992a51
ballfish <lili668668@gmail.com>
2025-08-30 20:57:35 +0800

    fix: remove useless files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Vue3Lottie from 'vue3-lottie'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(Vue3Lottie)
 
 app.mount('#app')
